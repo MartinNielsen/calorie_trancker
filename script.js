94a656989ceaf8e030b376ba592cc2bf9cfd7bc1
Martin D. Nielsen <martin@plug-n-trade.com>
2025-06-26 21:34:52 +0200

    Adjusted to be PWA

--- script.js
+++ script.js
@@ -231,4 +231,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     checkApiKey();
     renderDailyLog();
+
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/sw.js').then(registration => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }).catch(err => {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
 });
